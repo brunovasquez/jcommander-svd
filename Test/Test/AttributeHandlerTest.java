87e35a0d792b06b7026075a84736e9af8f74e983
dgumucio <daniel.gumucio@jalasoft.com>
2016-05-23 09:33:04 -0400

    Update AttributeHandlerTest.java

--- Test/Test/AttributeHandlerTest.java
+++ Test/Test/AttributeHandlerTest.java
@@ -24,24 +24,7 @@ public class AttributeHandlerTest {
     
     public AttributeHandlerTest() {
     }
-    
-    @BeforeClass
-    public static void setUpClass() {
-    }
-    
-    @AfterClass
-    public static void tearDownClass() {
-    }
-    
-    @Before
-    public void setUp() {
-    }
-    
-    @After
-    public void tearDown() {
-    }
 
-    
     @Test
     public void attributeHandlerHasListAttributes()
     {
