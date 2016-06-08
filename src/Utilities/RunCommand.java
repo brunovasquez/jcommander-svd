0dcec8e7bf907afb4213d249ac4a9bb7ae071a50
vhuaytav <mel.ania867@gmail.com>
2016-06-07 21:31:01 -0300

    Changes added  for run command in Linux Machines
    Unit test didn't added yet.

--- src/Utilities/RunCommand.java
+++ src/Utilities/RunCommand.java
@@ -5,9 +5,11 @@
  */
 package Utilities;
 
+import com.jcraft.jsch.*;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Properties;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -49,13 +51,41 @@ public class RunCommand {
     }
     
     /**
-     * Method to run a command in Windows Machine
+     * Method to run a command in Linux Machine
+     * @param userName String for user name to be used for execute the command
+     * @param portServer port number to be used for execute the command
+     * @param password String for password to be used for execute the command
      * @param command String that contains the command to be executed
-     * @return result message from the command execution, it will be empty if the command didn't worked
+     * @param hostname String for the server hostname to be used to execute the command
+     * @return 
      */
-    static String runCommandLinux(String command) {        
-        //TODO complete Run command for Linux machines
+    static String runCommandLinux(String userName, String password, int portServer, String command, String hostname) {        
         String result = "";
+        JSch js = new JSch();        
+        try{
+                Session session = js.getSession(userName, hostname, portServer);
+                session.setPassword(password);
+                Properties config = new Properties();
+                config.put("StrictHostKeyChecking", "no");
+                session.setConfig(config);
+                session.connect();
+                Channel objChannel = session.openChannel("exec");
+                ChannelExec objChanelExec = (ChannelExec) objChannel;
+                objChanelExec.setCommand(command);
+                objChanelExec.setErrStream(System.err);
+                objChanelExec.connect();
+                BufferedReader reader = new BufferedReader(new InputStreamReader(objChanelExec.getInputStream()));
+                String line;
+                while ((line = reader.readLine()) != null) {
+                    result= result + line +"\n"; 
+                }
+                objChanelExec.disconnect();
+                session.disconnect();
+        }catch (JSchException | IOException ex)
+        {            
+            Logger.getLogger(RunCommand.class.getName()).log(Level.SEVERE, "It was not possible execute command remotely on Linux machine, exception: {0}", ex.toString());
+            result = null;
+        }
         return result;
     }
 }
