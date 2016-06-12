8cff857d2e2e0b36e2d5a1a3e99c8fe6c2bce8e9
PINTO SHIRLEY JHOVANA <jhoshi_1820@hotmail.com>
2016-06-12 17:07:42 -0400

    Dividing the panels.

---
+++ src/UI/JFrameJC.java
@@ -0,0 +1,49 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package UI;
+
+/**
+ * Class where the main JFrame's components are called and defined
+ * @author Shirley Pinto
+ */
+public class JFrameJC extends javax.swing.JFrame {
+    // Variables declaration - do not modify                     
+    private MenuBar jMenuBar;
+    private BodyPanel jPanelBody;
+    private ActionButtons jPanelButtons;
+    // End of variables declaration               
+
+    /**
+     * Creates new form UI
+     */
+    public JFrameJC() {
+        initComponents();
+    }
+
+    /**
+     * This method is called from within the constructor to initialize the form.
+     */
+     private void initComponents() {
+
+        jPanelButtons = new ActionButtons();
+        jPanelBody = new BodyPanel();
+        jMenuBar = new MenuBar();
+        
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        addWindowListener(new java.awt.event.WindowAdapter() {
+            public void windowOpened(java.awt.event.WindowEvent evt) {
+                jPanelBody.formWindowOpened(evt);
+            }
+        });
+        
+        getContentPane().add(jPanelButtons, java.awt.BorderLayout.PAGE_END);
+        getContentPane().add(jPanelBody, java.awt.BorderLayout.CENTER);
+
+        setJMenuBar(jMenuBar);
+
+        pack();
+    }
+}
